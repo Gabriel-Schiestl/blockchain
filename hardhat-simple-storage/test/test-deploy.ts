31e26beebc5443ae5a0801eda11ce579807e6eab
Gabriel Felipe Kuhn Schiestl <gabriel.schiestl25@hotmail.com>
2025-11-28 17:34:59 -0300

    fix: typing tests

--- hardhat-simple-storage/test/test-deploy.ts
+++ hardhat-simple-storage/test/test-deploy.ts
@@ -1,26 +1,27 @@
 import { expect } from "chai";
 import { network } from "hardhat";
+import { SimpleStorage } from "../types/ethers-contracts/SimpleStorage.js";
 
 const { ethers } = await network.connect();
 
 describe("SimpleStorage", function () {
-  this.beforeEach(async function () {
-    this.simpleStorage = await ethers.deployContract("SimpleStorage");
+  let simpleStorage: SimpleStorage;
+
+  beforeEach(async function () {
+    simpleStorage = await ethers.deployContract("SimpleStorage");
   });
 
   it("Should start with a favorite number of 0", async function () {
-    const currentValue = await this.simpleStorage.retrieve();
-    expect(currentValue).equals(0n);
+    const currentValue = await simpleStorage.retrieve();
+    expect(currentValue).to.equal(0n);
   });
 
-  // run only this test
-  it.only("Should update when we call store", async function () {
+  it("Should update when we call store", async function () {
     const expectedValue = 7n;
-    const transactionResponse = await this.simpleStorage.store(expectedValue);
+    const transactionResponse = await simpleStorage.store(expectedValue);
     await transactionResponse.wait();
 
-    const currentValue = await this.simpleStorage.retrieve();
-
-    expect(currentValue).equals(expectedValue);
+    const currentValue = await simpleStorage.retrieve();
+    expect(currentValue).to.equal(expectedValue);
   });
 });
